a4090c974a5a57e262cdef5dedf3bc6159e40cf3
IBra-dalas <81654522+IBra-dalas@users.noreply.github.com>
2021-12-22 21:07:41 +0100

    Add files via upload

---
+++ Gasgone.API/Controllers/FournisseurController.cs
@@ -0,0 +1,32 @@
+﻿using Gasgone.ORM.Business;
+using Gasgone.ORM.Data;
+using Gasgone.ORM.Service;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Gasgone.API.Controllers
+{
+    [ApiController]
+    public class FournisseurController : ControllerBase
+    {
+        private readonly DataContext _bdd;
+        public FournisseurController(DataContext bdd)
+        {
+            _bdd = bdd;
+        }
+
+
+
+        [HttpGet("/Fournisseurs")]
+        public IActionResult Get()
+        {
+            return Ok(new ServiceFournisseur(_bdd).Get());
+        }
+
+        [HttpPost("/AddFournisseur/")]
+        public IActionResult Add(Fournisseur model)
+        {
+            return Ok(new ServiceFournisseur(_bdd).Add(model));
+        }
+    }
+}
